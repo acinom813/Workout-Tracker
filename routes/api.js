0e267898c50913acf4e1c4dc294e51a400e1b586
Monica Hill <monica.r.hill@gmail.com>
2021-04-10 19:32:53 -0400

    fix bugs

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ const {Workout} = require ("../models");
     });
     
     router.put("/api/workouts/:id", ({ body, params }, res) => {
-        Workout.updateOne({_id: params.id}, {$push:{exercises:body}}
+        Workout.updateOne({_id: params.id}, {$push:{exercise:body}}
         )
 
         .then(dbWorkout => {
